0857f8ab95f5f3857f30fbdddf3c849646de6b8a
anne75 <annef.cognet@gmail.com>
2014-06-22 19:37:54 -0700

    initial commit

---
+++ server.R
@@ -0,0 +1,36 @@
+data("wine",package="FactoMineR")
+load("map_df")
+load("bkgd_map")
+library("ggmap")
+library("tree")
+library("mapproj")
+
+f.whichwine<-function(odor, aroma,tannin){
+  vin<-wine[,c(1,3,4,17)]
+  names(vin)<-c("label","odor","aroma","tannin")
+  levels(vin$label)<-c("Saumur","Bourgueil", "Chinon") #fix a syntax error
+  vintree<-tree(label~., data=vin)
+  f.scale<-function(x,y) {
+    x<-(min(y)+x*(max(y)-min(y))/10)
+    return(x)
+  }
+  newdata<-data.frame("odor"=f.scale(odor,vin$odor),"aroma"=f.scale(aroma,vin$aroma),
+                      "tannin"=f.scale(tannin,vin$tannin))
+  return(as.character(predict(vintree,newdata, type="class")))
+}
+
+shinyServer(
+    
+  function(input, output) {
+    
+    temp1<-reactive(f.whichwine(input$odor, input$aroma, input$tannin))
+    
+    output$aoc <- renderPrint({temp1()
+    })
+    output$graph <- renderPlot({ 
+      data<-aoc_list[[temp1()]]
+      bkgd + geom_polygon(data =data, aes(x = long, y = lat, group = group),
+                  colour = 'red', fill = 'red', alpha = .4, size = .3)
+    })
+  }
+)
---
+++ ui.R
@@ -0,0 +1,21 @@
+library(shiny)
+
+shinyUI(fluidPage(
+  titlePanel("Wine Guess Game"),
+  h4('Imagine you hold a glass of a Loire Valley red wine, answer the questions on the left, 
+     and the app will guess where it comes from (and show it) !'),
+  sidebarLayout(
+    sidebarPanel(
+    h5('Questions'),
+    p(em('values go from 0(low) to 10 (high)')),
+    sliderInput('tannin', 'How much tannins do you see',value = 5, min = 0, max = 10, step = 0.005,),
+    sliderInput('odor', 'How intense is the odor ',value = 5, min = 0, max = 10, step = 0.005,),
+    sliderInput('aroma', 'How intense are the aroma',value = 5, min = 0, max = 10, step = 0.005,),
+    submitButton('Submit')
+  ),
+  mainPanel(
+    verbatimTextOutput('aoc'),
+    plotOutput('graph')
+  )
+))
+)
